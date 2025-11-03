52f9afd10e0744756cc3825b0f42442138d01c87
Study <jiyong5565@gmail.com>
2025-11-03 14:09:37 +0100

    Create 04.cs

---
+++ C#WithDrawing/06. Event/04.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Sample4 : Form
+{
+    private Label lb1, lb2;
+
+    public static void Main()
+    {
+        Application.Run(new Sample4());
+    }
+    public Sample4()
+    {
+        this.Text = "Sample";
+        this.Width = 250; this.Height = 100;
+
+        lb1 = new Label();
+        lb1.Text = "Choose it with Arrow key(SK key)";
+        lb1.Width = this.Width;
+
+        lb2 = new Label();
+        lb2.Top = lb1.Bottom;
+
+        lb1.Parent = this;
+        lb2.Parent = this;
+
+        this.KeyDown += new KeyEventHandler(fm_KeyDown);
+
+    }
+    public void fm_KeyDown(Object sender, KeyEventArgs e)
+    {
+        String str;
+        if(e.KeyCode == Keys.Up)
+        {
+           str = "up";
+        }
+        else if(e.KeyCode == Keys.Down)
+        {
+           str = "down";
+        }
+        else if(e.KeyCode == Keys.Right)
+        {
+           str = "right";
+        }
+        else if(e.KeyCode == Keys.Left)
+        {
+           str = "left";
+        }
+        else
+        {
+           str = "다른 키";
+        }
+        lb2.Text = str + "입니다.";
+    }
+}
