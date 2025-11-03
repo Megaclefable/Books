12d6dbbb43b9320e6cf528273d6e098922d64d5a
Study <jiyong5565@gmail.com>
2025-11-03 14:04:57 +0100

    Create 02.cs

---
+++ C#WithDrawing/06. Event/02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Sample2 : Form
+{
+    private Label lb;
+    private Button bt;
+
+    public static void Main()
+    {
+        Application.Run(new Sample2());
+    }
+    public Sample2()
+    {
+        this.Text = "Sample";
+        this.Width = 250; 
+        this.Height = 100;
+
+        lb = new Label();
+        lb.Text = "Welcome!";
+        lb.Width = 150;
+        
+        bt = new Button();
+        bt.Text = "Purchase";
+        bt.Top = this.Top + lb.Height;
+        bt.Width = lb.Width;
+
+        bt.Parent = this;
+        lb.Parent = this;  
+        
+        bt.Click += new EventHandler(bt_Click);
+    }
+    public void bt_Click(Object sender, EventArgs e)
+    {
+        lb.Text = "Thanks!";
+    }
+}
