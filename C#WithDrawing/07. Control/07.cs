98f074d8d32453aad07ef50663a4cfe626908bd6
Study <jiyong5565@gmail.com>
2025-11-05 16:20:55 +0100

    Radio button.cs

---
+++ C#WithDrawing/07. Control/07.cs
@@ -0,0 +1,57 @@
+/*
+If the radio button is clicked, other options become mute.
+
+eventhandler parameter can be used both : Object and object  
+*/
+
+using System;
+using System.Windows.Forms;
+
+class Sample7 : Form
+{
+    private Label lb;
+    private RadioButton rb1, rb2;
+    private GroupBox gb;
+
+    public static void Main()
+    {
+        Application.Run(new Sample7());
+    }
+    public Sample7()
+    {
+        this.Text = "Sample";
+        this.Width = 300; this.Height = 200;
+
+        lb = new Label();
+        lb.Text = "Welcome";
+        lb.Dock = DockStyle.Top;
+
+        rb1 = new RadioButton();
+        rb2 = new RadioButton();
+
+        rb1.Text = "Car";
+        rb2.Text = "Truck";
+        rb1.Checked = true;
+
+        rb1.Dock = DockStyle.Left;
+        rb2.Dock = DockStyle.Right;
+
+        gb = new GroupBox();
+        gb.Text = "type";
+        gb.Dock = DockStyle.Bottom;
+
+        rb1.Parent = gb;
+        rb2.Parent = gb;
+
+        lb.Parent = this;
+        gb.Parent = this;
+
+        rb1.Click += new EventHandler(rb_Click);
+        rb2.Click += new EventHandler(rb_Click);
+    }
+    public void rb_Click(Object sender, EventArgs e)
+    {
+        RadioButton tmp = (RadioButton)sender;
+        lb.Text = tmp.Text + "is chosen";
+    }
+}
